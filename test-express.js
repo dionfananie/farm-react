a4df639f53c0ea31f783d66e16c502af7b31eb21
Dion Fananie <dionfananie@gmail.com>
2024-12-21 19:31:16 +0700

    test config

--- test-express.js
+++ test-express.js
@@ -1,8 +1,14 @@
+import { sleep } from "k6";
 import http from "k6/http";
 export const options = {
-  vus: 100,
-  duration: "5s",
+  stages: [
+    { duration: "2s", target: 10 },
+    { duration: "10s", target: 100 },
+    { duration: "20s", target: 250 },
+    { duration: "30s", target: 650 },
+  ],
 };
 export default function () {
   http.get("http://103.186.1.74:3000/");
+  sleep(1);
 }
