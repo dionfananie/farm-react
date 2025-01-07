ded6dc5aa101e506910f210f47f4122b7825ed36
Dion Fananie <dionfananie@gmail.com>
2025-01-07 22:14:17 +0700

    gitignore

--- farm.config.server.ts
+++ farm.config.server.ts
@@ -3,35 +3,36 @@ import { builtinModules } from "node:module";
 export default defineConfig({
   compilation: {
     input: {
-      index: "./src/index-server.tsx"
+      index: "./src/index-server.tsx",
     },
     output: {
       path: "./dist",
       targetEnv: "node",
       format: "cjs",
-      publicPath: "/"
+      publicPath: "/static/",
+      assetsFilename: "static/[resourceName].[hash].[ext]",
     },
     external: [...builtinModules.map((m) => `^${m}$`)],
     css: {
       prefixer: {
-        targets: ["last 2 versions", "Firefox ESR", "> 1%", "ie >= 11"]
-      }
+        targets: ["last 2 versions", "Firefox ESR", "> 1%", "ie >= 11"],
+      },
     },
     assets: {
-      mode: "browser"
-    }
+      mode: "browser",
+    },
   },
   server: {
-    port: 9001
+    port: 9001,
   },
   plugins: [
     [
       "@farmfe/plugin-react",
       {
         refresh: false,
-        development: false
-      }
+        development: false,
+      },
     ],
-    "@farmfe/plugin-sass"
-  ]
+    "@farmfe/plugin-sass",
+  ],
 });
